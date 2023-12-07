847d053679c8f3739acd1b7b7ecd7dd4891ed01d
YhanahiRosenthal <yhanahi16@gmail.com>
2023-12-07 18:55:17 -0300

    Footer created with MUI and Style-components

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,13 @@ import mercadoPago from "../assets/mercado-pago.png";
 import twi from "../assets/twitter.png";
 import facebook from "../assets/facebook.png";
 import insta from "../assets/instagram.png";
-import { Grid, Link, Divider, Typography } from '@mui/material';
-import { styled } from "@mui/material";
+import copy from "../assets/copyright.png";
+import { Grid, Link, Typography, styled } from '@mui/material';
 
 const Container = styled('div')({
-    backgroundColor:'#3a68eb',
+    width: '100%',
+    backgroundColor: 'rgb(0,185,189)',
+    background: 'linear-gradient(180deg, rgba(0,185,189,1) 0%, rgba(22,26,255,1) 100%)'
 })
 
 const ContainerSections = styled('div')({
@@ -26,14 +28,14 @@ const ContainerService = styled('div')({
     padding: '.5rem 0',
     width: '20%',
     height: '100%',
-    textAlign: 'start',
+    textAlign: 'center',
 })
 
 const ContainerImages = styled('div')({
     display: 'flex',
     justifyContent: 'center',
     gap: '3rem',
-    padding: '.5rem 0'
+    paddingTop: '.5rem'
 })
 
 const Images = styled('img')({
@@ -51,17 +53,31 @@ const GridP = styled(Grid)({
     display: 'flex',
     justifyContent: 'center',
     gap: '1rem',
-    marginBottom: '3rem'
+    marginBottom: '3rem',
+
+    '@media (max-width: 768px)': {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        gap: '0',
+        textAlign: 'center',
+        '& > *': {
+          flex: '1 1 50%',
+          maxWidth: '50%',
+          padding: '10px'
+        }
+      }
 })
 
 const LinkT = styled(Link)({
     color: '#fff',
-    fontSize: '.7rem',
+    fontSize: '.8rem',
+    fontWeight: '500',
     cursor: 'pointer',
-    transition: '.1s all',
+    transition: '.2s all',
+    textDecoration: 'none',
     '&:hover': {
-        color: '#e6e6e6'
-    }
+        textDecoration: 'underline'
+    },
 })
 
 const ContainerCards = styled('div')({
@@ -83,46 +99,73 @@ const ContainerInfo = styled('div')({
     width: '95%'
 })
 
+const Subtitle = styled('h3')({
+    color: '#fff',
+    '@media (max-width: 768px)': {
+        marginBottom:'-.1rem'
+      }
+})
+
+const LastText = styled(Typography)({
+    display: 'flex',
+    alignItems: 'center',
+    gap: '.5rem',
+    padding: '.5rem',
+    fontSize: '.8rem',
+    fontWeight: '600',
+    color: '#fff',
+    '@media (max-width: 768px)': {
+        fontSize: '.5rem',
+      }
+})
+
+const Copy = styled('img')({
+    width: '.9rem',
+    height: '.9rem',
+    filter: 'invert()',
+    '@media (max-width: 768px)': {
+        display: 'none'
+      }
+})
 
 export default function Footer() {
 
   return (
     <Container>
-        <ContainerSections className="q">
-            <ContainerImages className="q">
-                <Link href="https://es-la.facebook.com/"><Images src={facebook} alt="" /></Link>
-                <Link href="https://www.instagram.com/"><Images src={insta} alt="" /></Link>
-                <Link href="https://www.instagram.com/"><Images src={twi} alt="" /></Link>
+        <ContainerSections>
+            <ContainerImages>
+                <Link href="https://es-la.facebook.com/"><Images src={facebook} alt="Icon Facebook" /></Link>
+                <Link href="https://www.instagram.com/"><Images src={insta} alt="Icon Instagram" /></Link>
+                <Link href="https://www.instagram.com/"><Images src={twi} alt="Icon Twitter" /></Link>
             </ContainerImages>
-            <Divider className="custom-divider" />
-            <GridP container className="q">
+            <hr style={{width:'100%'}} />
+            <GridP container>
                 <ContainerService>
-                    <h3>Nosotros</h3>
+                    <Subtitle>Nosotros</Subtitle>
                     <LinkT to="/not found" className="link">Nuestra Empresa</LinkT>
                     <LinkT to="/not found" className="link">Trabaja con Nosotros</LinkT>
-                    <LinkT to="/not found" className="link">Nuestras Tiendas</LinkT>
                 </ContainerService>
                 <ContainerService>
-                    <h3>Compras</h3>
+                    <Subtitle>Compras</Subtitle>
                     <LinkT to="/not found" className="link">Guía de compras</LinkT>
                     <LinkT to="/not found" className="link">Preguntas Frecuentes</LinkT>
                 </ContainerService>
                 <ContainerService>
-                    <h3>Atención al cliente</h3>
+                    <Subtitle>Atención al cliente</Subtitle>
                     <LinkT to="/not found" className="link">Contacto</LinkT>
                     <LinkT to="/not found" className="link">Rastrear mi pedido</LinkT>
                 </ContainerService>
             </GridP>
-            <Divider style={{ margin: '20px 0', backgroundColor: 'red' }} />
-            <ContainerCards className="q">
+            <hr style={{width:'100%'}} />
+            <ContainerCards>
                 <Card src={visa} alt="Icon visa card" />
                 <Card src={masterCard} alt="Icon masterCard card" />
                 <Card src={mercadoPago} alt="Icon mercadoPago" />
             </ContainerCards>
-            <Divider className="custom-divider" />
-            <ContainerInfo className="q">
-                <Typography style={{ fontSize: '.8rem', color: '#fff' }}>Copyright 2023 Tannat Indumentaria</Typography>
-                <Typography style={{ fontSize: '.8rem', color: '#fff' }}>Developed by: Nimion Software Solutions</Typography>
+            <hr style={{width:'100%'}} />
+            <ContainerInfo>
+                <LastText><Copy src={copy} alt='Copyright' />Copyright 2023 Tannat Indumentaria</LastText>
+                <LastText>Developed by: Nimion Software Solutions</LastText>
             </ContainerInfo>
         </ContainerSections>
     </Container>
