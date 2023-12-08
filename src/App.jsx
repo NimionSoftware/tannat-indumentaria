75e9e1682df7a927f543b881995254e9439f803d
Cristian Lotorto <cristianlotorto@gmail.com>
2023-12-08 17:27:49 -0300

    product card creation and styled finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css';
 import Footer from './components/Footer';
+import ProductCard from './components/ProductCard';
+import img from './assets/campera-ufo.png'
 
 function App() {
   return (
     <div className="App">
       <h1>Tannat</h1>
+      <ProductCard imgId={img} productName={'Campera Boss'} productDescription={'Campera de un material muy fachero, si no sos fachero no te la pongas porque vas a desentonar.'} productSizes={'S, M, L'} productPrice={'$36999'}/>
       <Footer />
     </div>
   );
